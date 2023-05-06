c7783d23bfa7fdbd7b67effd7eb219629df2f244
hemakumar chinnaiah <hemakumar.1986@gmail.com>
2023-05-06 22:01:44 +0100

    initial commit

---
+++ HemakumarSeleniumFramework/src/test/java/hemakumar/tests/ErrorValidationsTest.java
@@ -0,0 +1,44 @@
+package hemakumar.tests;
+
+import org.testng.annotations.Test;
+
+import hemakumar.TestComponents.BaseTest;
+import hemakumar.TestComponents.Retry;
+import hemakumar.pageobjects.CartPage;
+import hemakumar.pageobjects.CheckoutPage;
+import hemakumar.pageobjects.ConfirmationPage;
+import hemakumar.pageobjects.ProductCatalogue;
+
+import org.testng.AssertJUnit;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ErrorValidationsTest extends BaseTest {
+
+	//Error validation...
+	@Test(groups = { "ErrorHandling" }, retryAnalyzer = Retry.class)
+	public void LoginErrorValidation() throws IOException, InterruptedException {
+
+		landingPage.loginApplication("hemakumar.1986@gmail.com", "Iamki000");
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+
+	}
+
+	@Test
+	public void ProductErrorValidation() throws IOException, InterruptedException {
+
+		String productName = "ZARA COAT 3";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("hemakumar.1986@gmail.com", "Iamking@000");
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
+
+	}
+
+}
