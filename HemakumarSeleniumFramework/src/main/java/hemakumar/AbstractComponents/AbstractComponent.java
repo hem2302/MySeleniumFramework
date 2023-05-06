313d3b5e07c07e23e416eb8db99c165473ec11b1
hemakumar chinnaiah <hemakumar.1986@gmail.com>
2023-05-06 23:00:44 +0100

    added comment in abstract class

--- HemakumarSeleniumFramework/src/main/java/hemakumar/AbstractComponents/AbstractComponent.java
+++ HemakumarSeleniumFramework/src/main/java/hemakumar/AbstractComponents/AbstractComponent.java
@@ -14,7 +14,7 @@ import hemakumar.pageobjects.CartPage;
 import hemakumar.pageobjects.OrderPage;
 
 public class AbstractComponent {
-	
+	//abstrach components
 	WebDriver driver;
 
 	public AbstractComponent(WebDriver driver) {
